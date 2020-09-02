b01b8d3b879ee58bbdbff1a6cc3bd796793ebfd2
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-09-02 12:13:18 +0900

    ログイン機能実装

--- db/migrate/20200827054532_devise_create_users.rb
+++ db/migrate/20200827054532_devise_create_users.rb
@@ -7,12 +7,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :password,           null: false
+      # t.string :password,           null: false
       t.string :first_name,         null: false
       t.string :last_name,          null: false
       t.string :first_name_kana,    null: false
       t.string :last_name_kana,     null: false
-      t.string :birthday,           null: false
+      t.date   :birthday,           null: false
 
 
 
