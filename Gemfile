cda14aeb0d6677d280b51b2c82d4a907d83d8c71
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-09-10 15:26:25 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -68,5 +68,8 @@ gem 'active_hash'
 
 gem 'devise'
 
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
+
 
 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :not_sign_in, except:[:index]
+
+  def new
+  end
+
+  def create
+  end
+
   def index
+  end
+
+  def not_sign_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product_name, :price, :product_description, :Category_id, :ProductStatus_id, :DeliveryFee_id, :ShippingArea_id, :ShippingData_id).merge(user_id: current_user.id)
   end
 end
