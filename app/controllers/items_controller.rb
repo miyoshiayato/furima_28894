027f47ae3a5f415cd08e7c521c5a5ddee0a37694
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-09-14 21:01:09 +0900

    レビューでの指摘箇所修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :not_sign_in, except: [:index]
   def index
-    @item = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
