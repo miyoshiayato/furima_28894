7105e0db718a53cd18e78313ca8acb72a97fa1fc
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-09-16 19:45:00 +0900

    orederコントローラー・モデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root to: "items#index"
   resources :users, only: [:edit, :update]
   resources :items
+  resources :orders, only:[:create]
+
 end
