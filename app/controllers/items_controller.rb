de954a2fe7a7f5fd86a7c5581800fa5f62b4942a
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-09-16 13:09:30 +0900

    レビューでの指摘箇所修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :not_sign_in, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit]
+
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -19,17 +22,22 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
     item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to root_path
+    if item.update(item_params)
+       redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 
