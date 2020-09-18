3ccd1e41e83ec6f2a8d0dded5a96fef32a801f11
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-09-18 12:18:32 +0900

    purchases作成

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,39 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @purchase = PurchaseAddress.new
+    redirect_to root_path if @item.purchase || user_signed_in? && current_user.id == @item.user.id
+  end
+
+  def create
+    @purchase = PurchaseAddress.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      redirect_to root_path
+    else
+      redirect_to item_path(@item)
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.permit(:post_number, :prefecture_id, :city, :house_number, :building_name, :phone_number, :item_id, :token).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
---
+++ app/forms/purchase_address.rb
@@ -0,0 +1,23 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :token, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :post_number, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone_number, numericality: { only_integer: true },
+                             length: { maximum: 11 }
+    validates :token
+
+    with_options numericality: { other_than: 1 } do
+      validates :prefecture_id
+    end
+  end
+
+  def save
+    purchase = Purchase.create!(user_id: user_id, item_id: item_id)
+    Address.create!(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
