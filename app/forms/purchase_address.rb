59f00c99a8a2c15a2e3326c1d082c6ee73cbd193
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-09-21 20:47:16 +0900

    PurchaseAddress、バリデーション追記

--- app/forms/purchase_address.rb
+++ app/forms/purchase_address.rb
@@ -4,7 +4,7 @@ class PurchaseAddress
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :house_number
     validates :phone_number, numericality: { only_integer: true }, length: { maximum: 11 }
